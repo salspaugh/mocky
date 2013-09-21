8943759b15e60ce9c3b17e7c283b0e7b50ea6cd2
Sara Alspaugh <saraalspaugh@gmail.com>
2013-09-20 17:29:02 -0700

    Fixed the problem that was preventing Vega visualizations from working properly -- the issue was that you can't start a class or id attribute with a number.

--- mocky/api.py
+++ mocky/api.py
@@ -4,8 +4,9 @@ import copy
 import json
 
 def generate_recommendations():
+    # TODO: FIXME
     with open("mocky/data/square.vega") as spec:
         #square = json.load(spec)
         square = spec.read()
         print square
-        return [copy.deepcopy(square)]*4
+        return [copy.deepcopy(square)]*12
