b1c21ff4239db310ce99b721dea81c9853fb1bc7
Sara Alspaugh <saraalspaugh@gmail.com>
2013-09-23 15:17:00 -0700

    Made the API module return random-colored squares (fake recommendations).

--- mocky/api.py
+++ mocky/api.py
@@ -2,11 +2,23 @@ from mocky import app
 
 import copy
 import json
+import random
 
 def generate_recommendations():
-    # TODO: FIXME
+    squares = []
     with open("mocky/data/square.vega") as spec:
-        #square = json.load(spec)
-        square = spec.read()
-        print square
-        return [copy.deepcopy(square)]*12
+        square = json.load(spec)
+        for i in range(12):
+            s = copy.deepcopy(square)
+            s["marks"][0]["properties"]["update"]["fill"]["value"] = random_color()
+            s = json.dumps(s)
+            squares.append(s)
+    return squares
+
+def random_color():
+    R = str(hex(random.randint(0, 255))) 
+    G = str(hex(random.randint(0, 255)))
+    B = str(hex(random.randint(0, 255)))
+    color = "".join(['#', R[2:], G[2:], B[2:]])
+    return color
+
