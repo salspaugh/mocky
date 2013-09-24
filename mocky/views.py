1f8400ff6574ad678febd9654981d6bd725d4ba5
Sara Alspaugh <saraalspaugh@gmail.com>
2013-09-23 21:55:45 -0700

    Added new buttons. Made it so the drop down button gives the option of picking recommendations with similar colors.

--- mocky/views.py
+++ mocky/views.py
@@ -1,11 +1,14 @@
 
-from flask import g, render_template, url_for
+from flask import g, request, render_template, url_for
 from werkzeug import secure_filename
 
 from mocky import app
 from mocky import api
 
-@app.route("/")
-def index():
-    recommendations = api.generate_recommendations() 
+@app.route("/", methods=["GET", "POST"])
+def recommendations():
+    if request.method == "GET":
+        recommendations = api.recommendations() 
+    elif request.method == "POST":
+        recommendations = api.recommendations_like_this(request.form["data"])
     return render_template("index.html", recommendations=recommendations)
