7c041aeca1c405d7c340b739c04084bcd1e4a7bb
Sara Alspaugh <saraalspaugh@gmail.com>
2013-09-29 22:47:48 -0700

    Reformulated recommendations data structure that gets passed around.

--- mocky/api.py
+++ mocky/api.py
@@ -9,6 +9,8 @@ from os.path import isfile, join
 
 def recommendations():
     recpath = "recommendations"
-    recs = [ f for f in listdir(recpath) if isfile(join(recpath, f)) and f[-4:] == "vega" ]
-    recs = [json.dumps(json.load(open(join(recpath, r)))) for r in recs]
+    recs = [ f for f in listdir(recpath) if isfile(join(recpath, f)) and f[-4:] == ".rec" ]
+    recs = [json.load(open(join(recpath, r))) for r in recs]
+    recs = filter(lambda x: x.get("display", 0), recs)
+    recs = [json.dumps(r) for r in recs]
     return recs
